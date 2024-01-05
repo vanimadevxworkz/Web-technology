f46858a83b1d3b0aaa1a7c06d0deb4550aeb5015
vanimadevxworkz <vanimadevxworkz@gmail.com>
2024-01-05 10:42:17 +0530

    javaScript

---
+++ JavaScript.js
@@ -0,0 +1,177 @@
+var ref=20;
+console.log(ref);
+
+//declare only one time but initialise multiple time
+let city="benglore";
+city="bengalore";
+console.log(city);
+
+//declare and assign ony one time
+const price=100;
+console.log(price);
+
+const price2=300;
+console.log(price2);
+
+//number
+var ref=10;
+console.log(typeof(ref));
+
+var value=25;
+console.log(typeof(value));
+
+var ref2=30;
+console.log(typeof(ref2));
+
+var cost=45;
+console.log(typeof(cost));
+
+var cost2=200;
+console.log(typeof(cost2));
+
+var value2=250;
+console.log(typeof(value2));
+
+var value3=500;
+console.log(typeof(value3));
+
+ var cost=550;
+ console.log(typeof(cost));
+
+ var cost=100;
+ console.log(typeof(cost));
+
+
+ var cost=990;
+ console.log(typeof(cost));
+
+
+//String value
+
+let placeName="benglore";
+console.log(typeof(placeName));
+
+let placeName1="mysore";
+console.log(typeof(placeName1));
+
+let placeName3="bhatkal";
+console.log(typeof(placeName3));
+
+let placeName4="karwar";
+console.log(typeof(placeName4));
+
+let placeName5="manglore";
+console.log(typeof(placeName5));
+
+let Food="biriyani";
+console.log(typeof(Food));
+
+let food2="fridrice";
+console.log(typeof(food2));
+
+let food3="icecream";
+console.log(typeof(food3));
+
+let food4="noodles";
+console.log(typeof(food4));
+
+let food5="fish";
+console.log(typeof(food5));
+
+//boolean
+
+const isPlace=true;
+console.log(typeof(isPlace));
+
+const isFood=false;
+console.log(typeof(isFood));
+
+const placeOrder=true;
+console.log(typeof(placeOrder));
+
+const good=true;
+console.log(typeof(good));
+
+const ispayment=true;
+console.log(typeof(ispayment));
+
+const isnice=false;
+console.log(typeof(isnice));
+
+
+const nice=false;
+console.log(typeof(nice));
+
+const isName=true;
+console.log(typeof(isName));
+
+const place=true;
+console.log(typeof(place));
+
+const isVisited=true;
+console.log(typeof(isVisited));
+
+//undefine
+
+let payment;
+console.log(typeof(payment));
+
+var num;
+console.log(typeof(num));
+
+var number;
+console.log(typeof(number));
+
+var num2;
+console.log(typeof(num2));
+
+let payment2;
+console.log(typeof(payment2));
+
+let pay;
+console.log(typeof(pay));
+
+let mobileName;
+console.log(typeof(mobileName));
+
+let placeName2;
+console.log(typeof(placeName2));
+
+let house;
+console.log(typeof(house));
+
+var amount;
+console.log(typeof(amount));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
